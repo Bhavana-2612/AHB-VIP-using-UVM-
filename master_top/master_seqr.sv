f1c0f704e4a2cc0175ddf07f24ab8180680a52e0
Bhavana-2612 <143953527+Bhavana-2612@users.noreply.github.com>
2026-09-17 15:59:59 +0530

    Create master_seqr.sv

---
+++ master_top/master_seqr.sv
@@ -0,0 +1,7 @@
+class master_seqr extends uvm_sequencer #(master_xtn);
+        `uvm_component_utils(master_seqr)
+
+        function new(string name = "master_seqr",uvm_component parent);
+                super.new(name,parent);
+        endfunction
+endclass
